13333c27007a4a533ac50d56956d2ea733926b05
16ren <c_h_e_n_l_e_i@126.com>
2015-01-19 12:36:26 +0800

    添加网络连接判断类NetworkConnections
    Signed-off-by: 16ren <c_h_e_n_l_e_i@126.com>

--- src/com/ych/parkshare/StartupActivity.java
+++ src/com/ych/parkshare/StartupActivity.java
@@ -13,7 +13,6 @@ public class StartupActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		setContentView(R.layout.activity_startup);
-		
+		setContentView(R.layout.activity_startup);	
 	}
 }
