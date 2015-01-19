c5adf336118e55e2e8746cfc037b9af93a25a841
16ren <c_h_e_n_l_e_i@126.com>
2015-01-19 17:03:24 +0800

    修改登陆界面
    Signed-off-by: 16ren <c_h_e_n_l_e_i@126.com>

--- src/com/ych/parkshare/LogInActivity.java
+++ src/com/ych/parkshare/LogInActivity.java
@@ -1,17 +1,53 @@
 package com.ych.parkshare;
 
+import com.ych.views.LinkClickTextView;
+
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.Button;
 
 public class LogInActivity extends Activity {
+
+	private LinkClickTextView linkClickTextView;
+	private Button buttonSignup;
+	private Button buttonLogin;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_log_in);
+		linkClickTextView = (LinkClickTextView) findViewById(R.id.forgetpassword);
+		buttonLogin = (Button) findViewById(R.id.buttonlogin);
+		buttonSignup = (Button) findViewById(R.id.buttonsignup);
+		linkClickTextView.setOnClickListener(onClickListener);
+		buttonSignup.setOnClickListener(onClickListener);
+		buttonLogin.setOnClickListener(onClickListener);
 	}
+
+	private View.OnClickListener onClickListener = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			switch (v.getId()) {
+			case R.id.buttonlogin:
+
+				break;
+			case R.id.buttonsignup:
+
+				break;
+			case R.id.forgetpassword:
+
+				break;
+
+			default:
+				break;
+			}
+		}
+	};
 }
---
+++ src/com/ych/tool/Constant.java
@@ -0,0 +1,9 @@
+package com.ych.tool;
+
+import android.net.NetworkInfo.State;
+
+public class Constant {
+	public final static String USER_NAME="username";
+	public final static String USER_PASSWORD="userpassword";
+	public final static String USER_LOGIN="login";
+}
