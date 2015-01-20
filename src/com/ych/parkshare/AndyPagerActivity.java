b39431b09ae3c3e0c28b65bdb495d54402812a6e
16ren <c_h_e_n_l_e_i@126.com>
2015-01-20 17:08:09 +0800

    添加引导界面
    Signed-off-by: 16ren <c_h_e_n_l_e_i@126.com>

---
+++ src/com/ych/parkshare/AndyPagerActivity.java
@@ -0,0 +1,170 @@
+package com.ych.parkshare;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class AndyPagerActivity extends Activity implements OnClickListener, OnPageChangeListener {
+
+	private ViewPager vp;
+	private ViewPagerAdapter vpAdapter;
+	private List<View> views;
+	private Button button;
+
+	// ��ͼƬ��Դ
+	private static final int[] pics = { R.drawable.guide1, R.drawable.guide2, R.drawable.guide3, R.drawable.guide4 };
+
+	// �ײ�С��ͼƬ
+	private ImageView[] dots;
+
+	// ��¼��ǰѡ��λ��
+	private int currentIndex;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_andy_pager);
+
+		views = new ArrayList<View>();
+
+		LinearLayout.LayoutParams mParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+		// ��ʼ����ͼƬ�б�
+		for (int i = 0; i < pics.length; i++) {
+			ImageView iv = new ImageView(this);
+			iv.setLayoutParams(mParams);
+			iv.setImageResource(pics[i]);
+			views.add(iv);
+		}
+		vp = (ViewPager) findViewById(R.id.viewpager);
+		// ��ʼ��Adapter
+		vpAdapter = new ViewPagerAdapter(views);
+		vp.setAdapter(vpAdapter);
+		// �󶨻ص�
+		vp.setOnPageChangeListener(this);
+		button = (Button) findViewById(R.id.button);
+
+		button.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent();
+				intent.setClass(AndyPagerActivity.this, StartupActivity.class);
+				startActivity(intent);
+				finish();
+			}
+		});
+
+		// ��ʼ���ײ�С��
+		initDots();
+
+	}
+
+	private void initDots() {
+		LinearLayout ll = (LinearLayout) findViewById(R.id.ll);
+
+		dots = new ImageView[pics.length];
+
+		// ѭ��ȡ��С��ͼƬ
+		for (int i = 0; i < pics.length; i++) {
+			dots[i] = (ImageView) ll.getChildAt(i);
+			dots[i].setEnabled(true);// ����Ϊ��ɫ
+			dots[i].setOnClickListener(this);
+			dots[i].setTag(i);// ����λ��tag������ȡ���뵱ǰλ�ö�Ӧ
+		}
+
+		currentIndex = 0;
+		dots[currentIndex].setEnabled(false);// ����Ϊ��ɫ����ѡ��״̬
+	}
+
+	/**
+	 * ���õ�ǰ����ҳ
+	 */
+	private void setCurView(int position) {
+		if (position < 0 || position >= pics.length) {
+			return;
+		}
+
+		vp.setCurrentItem(position);
+	}
+
+	/**
+	 * ��ֻ��ǰ��С���ѡ��
+	 */
+	private void setCurDot(int positon) {
+		if (positon < 0 || positon > pics.length - 1 || currentIndex == positon) {
+			return;
+		}
+
+		dots[positon].setEnabled(false);
+		dots[currentIndex].setEnabled(true);
+
+		currentIndex = positon;
+	}
+
+	// ������״̬�ı�ʱ����
+	@Override
+	public void onPageScrollStateChanged(int arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	// ����ǰҳ�汻����ʱ����
+	@Override
+	public void onPageScrolled(int arg0, float arg1, int arg2) {
+		// TODO Auto-generated method stub
+
+	}
+
+	// ���µ�ҳ�汻ѡ��ʱ����
+	@Override
+	public void onPageSelected(int arg0) {
+		// ���õײ�С��ѡ��״̬
+		setCurDot(arg0);
+		if (arg0 == 3) {
+			button.setVisibility(View.VISIBLE);
+
+		}
+	}
+
+	@Override
+	public void onClick(View v) {
+		int position = (Integer) v.getTag();
+		setCurView(position);
+		setCurDot(position);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
