d2ce014fb4686d66b48f81a8a0d47439a62bbb08
16ren <c_h_e_n_l_e_i@126.com>
2015-01-20 19:46:46 +0800

    添加引导界面 Signed-off-by: 16ren <c_h_e_n_l_e_i@126.com>

---
+++ src/com/ych/parkshare/AboutActivity.java
@@ -0,0 +1,12 @@
+package com.ych.parkshare;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AboutActivity extends Activity{
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_about);
+	}
+}
---
+++ src/com/ych/parkshare/ContactActivity.java
@@ -0,0 +1,12 @@
+package com.ych.parkshare;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ContactActivity extends Activity{
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_contact);
+	}
+}
