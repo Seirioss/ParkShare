c072cd05a0558a8d92aa1d9d82ef2c68375067d9
16ren <c_h_e_n_l_e_i@126.com>
2015-01-21 12:40:45 +0800

    在assets文件夹下面添加服务器API文档，
    Signed-off-by: 16ren <c_h_e_n_l_e_i@126.com>

--- src/com/ych/tool/WebInterface.java
+++ src/com/ych/tool/WebInterface.java
@@ -1,35 +1,35 @@
 package com.ych.tool;
+
 //网络访问接口
 public class WebInterface {
-	/*http://121.40.61.76:8080/parkManagementSystem/
+	/*
+	 * http://121.40.61.76:8080/parkManagementSystem/
+	 * login/
+	 *  参数：
+	 *  username 
+	 *  password
+	 *  返回： 
+	 *  0 success
+	 *  1 account disable or already login 
+	 *  2 username or password invalid
+	 *  3 invalid request method
+	 * 
+	 * 登出
+	 * logout/
+	 *  参数： 无 返回： 
+	 *  0 success 
+	 *  1 not login or error
 
-		login/
-
-		success
-		0
-		account disable or already login
-		1
-		username or password invalid 
-		2
-		invalid request method
-		3
-
-
-		logout/
-
-		success
-		0
-		not login or error	
-		1
-
-
-		register/
-
-		success
-		0
-		user already exist	
-		1
-		invalid request method
-		2
-		*/
+	 * register/ 
+	 * 参数：
+	 *  username
+	 *   password 
+	 *   email 
+	 *   phone
+	 * 
+	 * 返回： 
+	 * 0 success 
+	 * 1 user already exist 
+	 * 2 invalid request method
+	 */
 }
