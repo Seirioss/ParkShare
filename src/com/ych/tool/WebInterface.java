5c8860b581099877c9e676d287beba68fb9cdd50
16ren <c_h_e_n_l_e_i@126.com>
2015-01-21 10:46:11 +0800

    添加网络接口文件WebInterface
    Signed-off-by: 16ren <c_h_e_n_l_e_i@126.com>

---
+++ src/com/ych/tool/WebInterface.java
@@ -0,0 +1,34 @@
+package com.ych.tool;
+//网络访问接口
+public class WebInterface {
+	/*http://121.40.61.76:8080/parkManagementSystem/
+
+		login/
+
+		success
+		0
+		account disable or already login
+		1
+		username or password invalid 
+		2
+		invalid request method
+		3
+
+
+		logout/
+
+		success
+		0
+		not login or error	
+		1
+
+
+		register/
+
+		success
+		0
+		user already exist	
+		1
+		invalid request method
+		2
+}*/
