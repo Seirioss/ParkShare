9ab54377436071c6e83e6ba51290d22553508fa5
16ren <c_h_e_n_l_e_i@126.com>
2015-01-21 14:46:53 +0800

    添加注册功能
    Signed-off-by: 16ren <c_h_e_n_l_e_i@126.com>

--- src/com/ych/parkshare/RegisterActivity.java
+++ src/com/ych/parkshare/RegisterActivity.java
@@ -3,13 +3,18 @@ package com.ych.parkshare;
 import org.apache.http.Header;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import com.ych.http.AsyncHttpClient;
 import com.ych.http.JsonHttpResponseHandler;
 import com.ych.http.RequestParams;
+import com.ych.http.TextHttpResponseHandler;
+import com.ych.tool.AppConstants;
+import com.ych.tool.SpUtils;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Canvas;
 import android.graphics.ColorFilter;
@@ -32,7 +37,6 @@ public class RegisterActivity extends Activity {
 	private EditText pass2EditText;
 	private EditText phoneEditText;
 	private EditText emailEditText;
-	private boolean canregister=false;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -67,70 +71,70 @@ public class RegisterActivity extends Activity {
 		emailEditText = (EditText) findViewById(R.id.edittextregisteremail);
 		buttonRegister = (Button) findViewById(R.id.buttonregister);
 		buttonRegister.setOnClickListener(onClickListener);
-		nameEditText.setOnFocusChangeListener(onFocusChangeListener);
-		pass2EditText.setOnFocusChangeListener(onFocusChangeListener);
-		emailEditText.setOnFocusChangeListener(onFocusChangeListener);
-		phoneEditText.setOnFocusChangeListener(onFocusChangeListener);
 	}
 
 	private View.OnClickListener onClickListener = new OnClickListener() {
+
 		@Override
 		public void onClick(View v) {
-			if(canregister){
-				AsyncHttpClient client=new AsyncHttpClient();
-				RequestParams params=new RequestParams();
-				params.put("name", nameEditText.getText().toString());
-				client.post("", params, new JsonHttpResponseHandler(){
-					@Override
-					public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-						super.onSuccess(statusCode, headers, response);
-						try {
-							int result=response.getInt("result");
-							if(result==1){
-								Intent intent =new Intent(RegisterActivity.this,MainActivity.class);
+			final String name = nameEditText.getText().toString();
+			final String pass1 = pass1EditText.getText().toString();
+			String pass2 = pass2EditText.getText().toString();
+			String phone = phoneEditText.getText().toString();
+			String email = emailEditText.getText().toString();
+			if (name.equals("")) {
+				Toast.makeText(RegisterActivity.this, "用户名空", Toast.LENGTH_SHORT).show();
+				return;
+			}
+			if (pass1.equals("") || pass2.equals("")) {
+				Toast.makeText(RegisterActivity.this, "密码空", Toast.LENGTH_SHORT).show();
+				return;
+			}
+			if (!pass1.equals(pass2)) {
+				Toast.makeText(RegisterActivity.this, "密码不一致", Toast.LENGTH_SHORT).show();
+				return;
+			}
+
+			AsyncHttpClient client = new AsyncHttpClient();
+			RequestParams params = new RequestParams();
+			params.put("username", name);
+			params.put("password", pass1);
+			params.put("email", phone);
+			params.put("phone", email);
+
+			client.post("http://121.40.61.76:8080/parkManagementSystem/register/", params, new TextHttpResponseHandler("utf-8") {
+
+				@Override
+				public void onSuccess(int statusCode, Header[] headers, String responseString) {
+					if (responseString.equals("0")) {
+						AlertDialog.Builder builder = new Builder(RegisterActivity.this);
+						builder.setTitle("注册成功");
+						builder.setMessage("是否登陆");
+						builder.setNegativeButton("取消", null);
+						builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+							@Override
+							public void onClick(DialogInterface dialog, int which) {
+								SpUtils.put(getApplicationContext(), AppConstants.USER_LOGIN, true);
+								SpUtils.put(getApplicationContext(), AppConstants.USER_REMEMBER, true);
+								SpUtils.put(getApplicationContext(), AppConstants.USER_NAME, name);
+								SpUtils.put(getApplicationContext(), AppConstants.USER_PASSWORD, pass1);
+								Intent intent = new Intent(RegisterActivity.this, TabHostActivity.class);
 								startActivity(intent);
 								RegisterActivity.this.finish();
 							}
-						} catch (JSONException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-						
+						});
+						builder.create().show();
 					}
-				});
-			}else {
-				Toast.makeText(RegisterActivity.this,"信息不正确", 0).show();
-			}
-		}
-	};
-	private View.OnFocusChangeListener onFocusChangeListener = new OnFocusChangeListener() {
+					if (responseString.equals("1")) {
+						Toast.makeText(RegisterActivity.this, "用户名已近存在", Toast.LENGTH_SHORT).show();
+					}
+				}
 
-		@Override
-		public void onFocusChange(View v, boolean hasFocus) {
-			switch (v.getId()) {
-			case R.id.edittextregistername:
-				if (!hasFocus) {
-					// 检测用户名是否已经被注册，字符范围是否有特殊字符
+				@Override
+				public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+					Toast.makeText(RegisterActivity.this, "网络故障", Toast.LENGTH_SHORT).show();
 				}
-				break;
-			case R.id.edittextregisterpasstwo:
-				if (hasFocus) {
-					//检测两次的密码是否一致
-				}
-				break;
-			case R.id.edittextregisterphone:
-				if (hasFocus) {
-					//检测手机号是否为空，号码是否正确
-				}
-				break;
-			case R.id.edittextregisteremail:
-				if (hasFocus) {
-					//检测email格式时候正确
-				}
-				break;
-			default:
-				break;
-			}
+			});
 		}
 	};
 
@@ -139,6 +143,4 @@ public class RegisterActivity extends Activity {
 		finish();
 		return super.onOptionsItemSelected(item);
 	}
-	
 }
-
