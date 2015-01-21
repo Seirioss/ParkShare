8cd4f747db89a8b1455e2ee7c5a8094a950254d4
lenovo <lenovo@Lenovo-PC>
2015-01-21 14:45:50 +0800

    //

--- src/com/ych/parkshare/TabHomeActivity.java
+++ src/com/ych/parkshare/TabHomeActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class TabHomeActivity extends Activity{
 	@Override
+	//
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
