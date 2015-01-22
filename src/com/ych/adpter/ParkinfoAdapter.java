8a60f6a1d3184e4fe15a37bcfd883d44bf1142eb
16ren <c_h_e_n_l_e_i@126.com>
2015-01-22 12:55:49 +0800

    添加下拉刷新listview
    Signed-off-by: 16ren <c_h_e_n_l_e_i@126.com>

---
+++ src/com/ych/adpter/ParkinfoAdapter.java
@@ -0,0 +1,16 @@
+package com.ych.adpter;
+
+import java.util.List;
+import java.util.Map;
+
+import android.content.Context;
+import android.widget.SimpleAdapter;
+
+public class ParkinfoAdapter extends SimpleAdapter {
+
+	public ParkinfoAdapter(Context context, List<? extends Map<String, ?>> data, int resource, String[] from, int[] to) {
+		super(context, data, resource, from, to);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
--- src/com/ych/parkshare/TabHomeActivity.java
+++ src/com/ych/parkshare/TabHomeActivity.java
@@ -1,7 +1,13 @@
 package com.ych.parkshare;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.http.Header;
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.ych.http.AsyncHttpClient;
@@ -15,21 +21,33 @@ import com.ych.tool.GlobalVariable;
 import com.ych.views.RefreshableView;
 import com.ych.views.RefreshableView.PullToRefreshListener;
 
+import android.R.integer;
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
 import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.Toast;
 //fdfsdf
 //123
+
 public class TabHomeActivity extends Activity {
 	RefreshableView refreshableView;
 	ListView listView;
-	ArrayAdapter<String> adapter;
+	private ListAdapter listAdapter;
+	private final static int UPDATE=1;
 	String[] items = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L" };
+	String[] from =new String[]{"pk","address","comment","describe","owner"};
+	int[] to=new int[]{R.id.pk,R.id.address,R.id.comment,R.id.describe,R.id.owner};
 	private SyncHttpClient client;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -38,8 +56,16 @@ public class TabHomeActivity extends Activity {
 	
 		refreshableView = (RefreshableView) findViewById(R.id.refreshable_view);
 		listView = (ListView) findViewById(R.id.list_view);
-		adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
-		listView.setAdapter(adapter);
+		listView.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+			@Override
+			public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+			}
+
+			@Override
+			public void onNothingSelected(AdapterView<?> parent) {
+			}
+		});
 		refreshableView.setOnRefreshListener(new PullToRefreshListener() {
 			@Override
 			public void onRefresh() {
@@ -51,18 +77,69 @@ public class TabHomeActivity extends Activity {
 					@Override
 					public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
 						super.onSuccess(statusCode, headers, response);
-						System.out.println(response.toString());
+						try {
+							int status=response.getInt("status");
+							if(status==0){
+								System.out.println(response.toString());
+								JSONArray jsonArray=response.getJSONArray("parks");
+								List<Map<String, String>> list =jsonArraytoList(jsonArray);
+								listAdapter=new SimpleAdapter(TabHomeActivity.this, list, R.layout.item_parkinfo, from, to);
+								Message message=uihHandler.obtainMessage();
+								message.what=UPDATE;
+								message.sendToTarget();
+							}
+						} catch (JSONException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
 					}
 
 					@Override
 					public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
 						super.onFailure(statusCode, headers, throwable, errorResponse);
-						System.out.println("user/park/ error");
+						System.out.println(throwable.toString());
 					}
 					
 				});
 				refreshableView.finishRefreshing();
 			}
 		}, 0);
+	}
+	
+	private Handler uihHandler=new Handler(){
+
+		@Override
+		public void handleMessage(Message msg) {
+			switch (msg.what) {
+			case UPDATE:
+				listView.setAdapter(listAdapter);
+				break;
+
+			default:
+				break;
+			}
+			super.handleMessage(msg);
+		}
+		
+	};
+	private List<Map<String, String>> jsonArraytoList(JSONArray jsonArray) {
+		List<Map<String, String>> list = new ArrayList<Map<String, String>>(); 
+		try {
+			for (int i = 0; i < jsonArray.length(); i++) {
+				JSONObject jsonObject=jsonArray.getJSONObject(i);
+				Map<String, String> map = new HashMap<String, String>(); 
+				map.put("pk", jsonObject.getString("pk"));
+				JSONObject tempJsonObject=jsonObject.getJSONObject("fields");
+				map.put("address", tempJsonObject.getString("address"));
+				map.put("comment", tempJsonObject.getString("comment"));
+				map.put("describe", tempJsonObject.getString("describe"));
+				map.put("owner", tempJsonObject.getString("owner"));
+				list.add(map);
+			}
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return list;
 	}
 }
