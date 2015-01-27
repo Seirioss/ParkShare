1163d40185021301c25c621433e0a3fa1eafac79
joiecen <cen_jiayin@163.com>
2015-01-27 17:03:20 +0800

    添加一键导航测试

---
+++ src/com/ych/parkshare/BNavigatorActivity.java
@@ -0,0 +1,164 @@
+package com.ych.parkshare;
+
+import com.baidu.navisdk.BaiduNaviManager;
+import com.baidu.navisdk.comapi.mapcontrol.BNMapController;
+import com.baidu.navisdk.comapi.routeplan.BNRoutePlaner;
+import com.baidu.navisdk.comapi.tts.BNTTSPlayer;
+import com.baidu.navisdk.comapi.tts.BNavigatorTTSPlayer;
+import com.baidu.navisdk.model.datastruct.LocData;
+import com.baidu.navisdk.model.datastruct.SensorData;
+import com.baidu.navisdk.ui.routeguide.BNavigator;
+import com.baidu.navisdk.ui.routeguide.IBNavigatorListener;
+import com.baidu.navisdk.ui.widget.RoutePlanObserver;
+import com.baidu.nplatform.comapi.map.MapGLSurfaceView;
+
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class BNavigatorActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_bnavigator);
+		
+		//baidumap
+		//创建NmapView  
+        MapGLSurfaceView nMapView = BaiduNaviManager.getInstance().createNMapView(this);  
+ 
+        //创建导航视图  
+        View navigatorView = BNavigator.getInstance().init(BNavigatorActivity.this, getIntent().getExtras(), nMapView);  
+ 
+        //填充视图  
+        setContentView(navigatorView);  
+ 
+        BNavigator.getInstance().setListener(mBNavigatorListener);  
+        BNavigator.getInstance().startNav();    
+                
+//        BNRoutePlaner.getInstance().setObserver(new RoutePlanObserver(this, new IJumpToDownloadListener() {  
+// 
+//            @Override  
+//            public void onJumpToDownloadOfflineData() {  
+//                // TODO Auto-generated method stub  
+// 
+//            }  
+//        }));  
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.bnavigator, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+	
+	//baidumap
+	private IBNavigatorListener mBNavigatorListener = new IBNavigatorListener() {  
+		 
+        @Override  
+        public void onYawingRequestSuccess() {  
+            // TODO 偏航请求成功  
+ 
+        }  
+ 
+        @Override  
+        public void onYawingRequestStart() {  
+            // TODO 开始偏航请求  
+ 
+        }  
+ 
+        @Override  
+        public void onPageJump(int jumpTiming, Object arg) {  
+            // TODO 页面跳转回调  
+            if(IBNavigatorListener.PAGE_JUMP_WHEN_GUIDE_END == jumpTiming){
+            	finish();
+            }else if(IBNavigatorListener.PAGE_JUMP_WHEN_ROUTE_PLAN_FAIL == jumpTiming){  
+                finish();  
+            }  
+        }  
+ 
+        @Override  
+        public void notifyGPSStatusData(int arg0) {  
+            // TODO Auto-generated method stub  
+ 
+        }  
+ 
+        @Override  
+        public void notifyLoacteData(LocData arg0) {  
+            // TODO Auto-generated method stub  
+ 
+        }  
+ 
+        @Override  
+        public void notifyNmeaData(String arg0) {  
+            // TODO Auto-generated method stub  
+ 
+        }  
+ 
+        @Override  
+        public void notifySensorData(SensorData arg0) {  
+            // TODO Auto-generated method stub  
+ 
+        }  
+ 
+        @Override  
+        public void notifyStartNav() {  
+            // TODO Auto-generated method stub  
+            BaiduNaviManager.getInstance().dismissWaitProgressDialog();  
+        }  
+ 
+        @Override  
+        public void notifyViewModeChanged(int arg0) {  
+            // TODO Auto-generated method stub  
+ 
+        }  
+ 
+	};
+
+	@Override  
+	public void onResume() {  
+	    BNavigator.getInstance().resume();  
+	    super.onResume();  
+	    BNMapController.getInstance().onResume();  
+	};  
+	
+	@Override  
+	public void onPause() {  
+	    BNavigator.getInstance().pause();  
+	    super.onPause();  
+	    BNMapController.getInstance().onPause();  
+	}  
+	
+	@Override  
+	public void onConfigurationChanged(Configuration newConfig) {  
+	    BNavigator.getInstance().onConfigurationChanged(newConfig);  
+	    super.onConfigurationChanged(newConfig);  
+	}  
+	
+	public void onBackPressed(){  
+	    BNavigator.getInstance().onBackPressed();  
+	}  
+	
+	@Override  
+	public void onDestroy(){  
+	    BNavigator.destory();  
+	    BNRoutePlaner.getInstance().setObserver(null);  
+	    super.onDestroy();  
+	}
+}
--- src/com/ych/parkshare/MainActivity.java
+++ src/com/ych/parkshare/MainActivity.java
@@ -4,6 +4,9 @@ import java.net.URI;
 
 import org.apache.http.Header;
 
+import com.baidu.lbsapi.auth.LBSAuthManagerListener;
+import com.baidu.navisdk.BNaviEngineManager.NaviEngineInitListener;
+import com.baidu.navisdk.BaiduNaviManager;
 import com.ych.http.AsyncHttpClient;
 import com.ych.http.TextHttpResponseHandler;
 import com.ych.image.SmartImage;
@@ -16,12 +19,16 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewDebug;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	private boolean mIsEngineInitSuccess = false; //baidumap
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -37,5 +44,44 @@ public class MainActivity extends Activity {
 			startActivity(intent);
 		}
 		this.finish();
+		
+		//初始化导航引擎  
+        BaiduNaviManager.getInstance().  
+            initEngine(this, getSdcardDir(), mNaviEngineInitListener,new LBSAuthManagerListener() {
+				
+				@Override
+				public void onAuthResult(int status, String msg) {
+					// TODO Auto-generated method stub
+					String str = null;
+					if (0 == status) {
+                        str = "key校验成功!";
+                    } else {
+                        str = "key校验失败, " + msg;
+                    }
+                    Toast.makeText(MainActivity.this, str,
+                            Toast.LENGTH_LONG).show();
+				}
+			});
 	}
+	
+	//baidumap
+	private NaviEngineInitListener mNaviEngineInitListener = new NaviEngineInitListener() {  
+        public void engineInitSuccess() {  
+            //导航初始化是异步的，需要一小段时间，以这个标志来识别引擎是否初始化成功，为true时候才能发起导航  
+            mIsEngineInitSuccess = true;  
+        }  
+ 
+        public void engineInitStart() {  
+        }  
+ 
+        public void engineInitFail() {  
+        }  
+    };  
+private String getSdcardDir() {  
+        if (Environment.getExternalStorageState().equalsIgnoreCase(  
+                Environment.MEDIA_MOUNTED)) {  
+            return Environment.getExternalStorageDirectory().toString();  
+        }  
+        return null;  
+    }  
 }
