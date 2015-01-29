8066692a0dd55c74a458ff6ae6c5d847454bc200
16ren <c_h_e_n_l_e_i@126.com>
2015-01-29 16:06:32 +0800

    添加推送模块
    Signed-off-by: 16ren <c_h_e_n_l_e_i@126.com>

---
+++ src/com/ych/receivers/BaiduPushMessageReceiver.java
@@ -0,0 +1,15 @@
+package com.ych.receivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BaiduPushMessageReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		// TODO Auto-generated method stub
+		System.out.println("-------------");
+	}
+
+}
