0df65a98a363669daec6e6dfdc9bbc04248971ff
16ren <c_h_e_n_l_e_i@126.com>
2015-02-03 18:02:17 +0800

    接收通知跳转到车位详情界面
    Signed-off-by: 16ren <c_h_e_n_l_e_i@126.com>

--- src/com/ych/receivers/BaiduPushMessageReceiver.java
+++ src/com/ych/receivers/BaiduPushMessageReceiver.java
@@ -3,83 +3,105 @@ package com.ych.receivers;
 import java.util.List;
 
 import org.apache.http.Header;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import com.baidu.frontia.api.FrontiaPushMessageReceiver;
 import com.ych.http.AsyncHttpClient;
 import com.ych.http.PersistentCookieStore;
 import com.ych.http.RequestParams;
 import com.ych.http.TextHttpResponseHandler;
+import com.ych.parkshare.ParkRentActivity;
+import com.ych.parkshare.StartupActivity;
 import com.ych.tool.AppConstants;
 import com.ych.tool.GlobalVariable;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.text.TextUtils;
 
-public class BaiduPushMessageReceiver extends FrontiaPushMessageReceiver{
-	
+public class BaiduPushMessageReceiver extends FrontiaPushMessageReceiver {
+
 	@Override
 	public void onBind(Context arg0, int arg1, String arg2, String arg3, String arg4, String arg5) {
 		// TODO Auto-generated method stub
-		AsyncHttpClient asyncHttpClient=new AsyncHttpClient();
+		AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
 		RequestParams params = new RequestParams();
 		params.add("pushuserid", arg3);
 		params.add("pushchannelid", arg4);
-		PersistentCookieStore persistentCookieStore=((GlobalVariable)arg0.getApplicationContext()).getPersistentCookieStore();
+		PersistentCookieStore persistentCookieStore = ((GlobalVariable) arg0.getApplicationContext()).getPersistentCookieStore();
 		asyncHttpClient.setCookieStore(persistentCookieStore);
-		asyncHttpClient.post(AppConstants.BASE_URL+AppConstants.URL_UPDATEPUSHINFO, params,updatepushinfoHttpResponseHandler);
+		asyncHttpClient.post(AppConstants.BASE_URL + AppConstants.URL_UPDATEPUSHINFO, params, updatepushinfoHttpResponseHandler);
 	}
 
 	@Override
 	public void onDelTags(Context arg0, int arg1, List<String> arg2, List<String> arg3, String arg4) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onListTags(Context arg0, int arg1, List<String> arg2, String arg3) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onMessage(Context arg0, String arg1, String arg2) {
 		// TODO Auto-generated method stub
-		System.out.println("message:arg1:"+arg1);
-		System.out.println("message:arg2:"+arg2);
-		
+		System.out.println("message:arg1:" + arg1);
+		System.out.println("message:arg2:" + arg2);
+
 	}
 
 	@Override
 	public void onNotificationClicked(Context arg0, String arg1, String arg2, String arg3) {
-		
+		try {
+			if (!TextUtils.isEmpty(arg3)) {
+				JSONObject jsonObject = new JSONObject(arg3);
+				if (!jsonObject.isNull("parkid")) {
+					String parkid = jsonObject.getString("parkid");
+					Intent intent = new Intent(arg0, ParkRentActivity.class);
+					intent.putExtra("pk", parkid);
+					intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+					arg0.getApplicationContext().startActivity(intent);
+				}
+			}
+
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 	@Override
 	public void onSetTags(Context arg0, int arg1, List<String> arg2, List<String> arg3, String arg4) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void onUnbind(Context arg0, int arg1, String arg2) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	
-	private TextHttpResponseHandler updatepushinfoHttpResponseHandler=new TextHttpResponseHandler("utf-8") {
-		
+	private TextHttpResponseHandler updatepushinfoHttpResponseHandler = new TextHttpResponseHandler("utf-8") {
+
 		@Override
 		public void onSuccess(int statusCode, Header[] headers, String responseString) {
 			// TODO Auto-generated method stub
 			System.out.println(responseString);
 		}
-		
+
 		@Override
 		public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
 			// TODO Auto-generated method stub
-			
+
 		}
 	};
 }
